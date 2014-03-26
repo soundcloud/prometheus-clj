9a4ee405db5b1462c92fd4840c723307b36b8575
Tom Czarniecki <thomas@soundcloud.com>
2014-03-26 11:04:01 -0400

    setting up for deployment to clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
-(def feature-version "1.0")
-(def build-version (or (System/getenv "BUILD_NUMBER") "local"))
-(def release-version (str feature-version "." build-version))
-
-(defproject com.soundcloud/prometheus-clj feature-version
+(defproject com.soundcloud/prometheus-clj "1.0-SNAPSHOT"
   :description "Clojure wrappers for the Prometheus java client"
   :url "https://github.com/soundcloud/prometheus-clj"
 
@@ -14,8 +10,6 @@
                  [com.matttproud.accepts/accepts "0.0.2"]]
 
   :min-lein-version "2.0.0"
-
-  :manifest {"Implementation-Version" ~release-version}
 
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [javax.servlet/servlet-api "2.5"]]
