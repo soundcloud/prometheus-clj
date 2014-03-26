5de0ae125487d5459627ee8e8e2eb024cd763adc
Tom Czarniecki <thomas@soundcloud.com>
2014-03-26 10:04:53 -0400

    Changed to Apache 2.0 license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prometheus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Clojure wrappers for the Prometheus java client"
+  :url "https://github.com/soundcloud/prometheus-clj"
+  :license {:name "The Apache Software License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
