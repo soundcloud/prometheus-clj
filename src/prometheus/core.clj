a3c219536f331028fc05f1fe85ee5273a03ed0e1
Tom Czarniecki <tomcz@users.noreply.github.com>
2015-11-03 00:46:54 -0500

    add type hits to remove reflection and speed up metrics recording

--- src/prometheus/core.clj
+++ src/prometheus/core.clj
@@ -3,12 +3,12 @@
     (clojure.lang IObj)
     (java.io StringWriter)
     (io.prometheus.client.exporter.common TextFormat)
-    (io.prometheus.client Counter Histogram CollectorRegistry)))
+    (io.prometheus.client Counter Histogram Counter$Child Histogram$Child CollectorRegistry)))
 
 ; more useful set of buckets for microservice APIs than the defaults provided by the Histogram class
 (def histogram-buckets (atom [0.001, 0.005, 0.010, 0.020, 0.050, 0.100, 0.200, 0.300, 0.500, 0.750, 1, 5]))
 
-(defn- make-request-counter [app-name registry]
+(defn- ^Counter make-request-counter [app-name registry]
   (-> (Counter/build)
       (.namespace app-name)
       (.name "http_requests_total")
@@ -16,23 +16,25 @@
       (.help "A counter of the total number of HTTP requests processed.")
       (.register registry)))
 
-(defn- make-request-histogram [app-name registry]
+(defn- ^Histogram make-request-histogram [app-name registry]
   (-> (Histogram/build)
+      (.buckets (double-array @histogram-buckets))
       (.namespace app-name)
       (.name "http_request_latency_seconds")
       (.labelNames (into-array String ["method" "status" "path"]))
       (.help "A histogram of the response latency for HTTP requests in seconds.")
-      (.buckets (double-array @histogram-buckets))
       (.register registry)))
+
+(defn- ^Counter$Child counter-with-labels [^Counter counter label-array] (.labels counter label-array))
+
+(defn- ^Histogram$Child histogram-with-labels [^Histogram histogram label-array] (.labels histogram label-array))
 
 (defn- record-request-metric [counter histogram request-method response-status request-time response-path]
   (let [method-label (.toUpperCase (name request-method))
         status-label (str (int (/ response-status 100)) "XX")
         labels (into-array String [method-label status-label response-path])]
-    (-> (.labels counter labels)
-        (.inc))
-    (-> (.labels histogram labels)
-        (.observe request-time))))
+    (-> (histogram-with-labels histogram labels) (.observe request-time))
+    (-> (counter-with-labels counter labels) (.inc))))
 
 (defn with-path
   "Adds the matched compojure route as the :path response metadata attribute"
