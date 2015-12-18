b61ecdddc76ff55b61f09da0efe440a5abe5981f
Joscha <joscha@schnipseljagd.org>
2015-12-18 14:40:01 +0100

    add support for custom metrics
    Adds counters, gauges and histograms.

--- src/prometheus/core.clj
+++ src/prometheus/core.clj
@@ -5,7 +5,7 @@
     (clojure.lang IObj)
     (java.io StringWriter)
     (io.prometheus.client.exporter.common TextFormat)
-    (io.prometheus.client Counter Histogram Counter$Child Histogram$Child CollectorRegistry)))
+    (io.prometheus.client Counter Histogram Counter$Child Histogram$Child CollectorRegistry Gauge Gauge$Child)))
 
 ; more useful set of buckets for microservice APIs than the defaults provided by the Histogram class
 (def histogram-buckets (atom [0.001, 0.005, 0.010, 0.020, 0.050, 0.100, 0.200, 0.300, 0.500, 0.750, 1, 5]))
@@ -27,16 +27,78 @@
       (.help "A histogram of the response latency for HTTP requests in seconds.")
       (.register registry)))
 
-(defn- ^Counter$Child counter-with-labels [^Counter counter label-array] (.labels counter label-array))
+(defn- ^Counter$Child counter-with-labels [^Counter counter label-array] (.labels counter (into-array String label-array)))
 
-(defn- ^Histogram$Child histogram-with-labels [^Histogram histogram label-array] (.labels histogram label-array))
+(defn- ^Gauge$Child gauge-with-labels [^Gauge gauge labels] (.labels gauge (into-array String labels)))
+
+(defn- ^Histogram$Child histogram-with-labels [^Histogram histogram label-array] (.labels histogram (into-array String label-array)))
 
 (defn- record-request-metric [counter histogram request-method response-status request-time response-path]
   (let [status-class (str (int (/ response-status 100)) "XX")
         method-label (string/upper-case (name request-method))
-        labels (into-array String [method-label (str response-status) status-class response-path])]
+        labels [method-label (str response-status) status-class response-path]]
     (-> (histogram-with-labels histogram labels) (.observe request-time))
     (-> (counter-with-labels counter labels) (.inc))))
+
+(defn- ^Counter make-counter [^CollectorRegistry registry namespace metric help label-names]
+  (-> (Counter/build)
+      (.namespace namespace)
+      (.name metric)
+      (.labelNames (into-array String label-names))
+      (.help help)
+      (.register registry)))
+
+(defn- ^Gauge make-gauge [^CollectorRegistry registry namespace metric help label-names]
+  (-> (Gauge/build)
+      (.namespace namespace)
+      (.name metric)
+      (.labelNames (into-array String label-names))
+      (.help help)
+      (.register registry)))
+
+(defn- ^Histogram make-histogram [^CollectorRegistry registry namespace metric help label-names buckets]
+  (-> (Histogram/build)
+      (.buckets (double-array buckets))
+      (.namespace namespace)
+      (.name metric)
+      (.labelNames (into-array String label-names))
+      (.help help)
+      (.register registry)))
+
+(defn set-gauge
+  "Set the value of a registered gauge."
+  [store namespace metric value & {:keys [labels] :or {labels []}}]
+  (-> (gauge-with-labels (get-in store [:metrics namespace metric]) labels)
+      (.set value)))
+
+(defn register-gauge
+  "Registers a gauge to the store and returns the new store."
+  [store namespace metric help label-names]
+  (assoc-in store [:metrics namespace metric] (make-gauge (:registry store) namespace metric help label-names)))
+
+(defn register-counter
+  "Registers a counter to the store and returns the new store."
+  [store namespace metric help label-names]
+  (assoc-in store [:metrics namespace metric] (make-counter (:registry store) namespace metric help label-names)))
+
+(defn increase-counter
+  "Increase the value of a registered counter."
+  [store namespace metric & {:keys [amount labels] :or {amount 1.0 labels []}}]
+  (-> (counter-with-labels (get-in store [:metrics namespace metric]) labels)
+      (.inc amount)))
+
+(defn register-histogram
+  "Registers a histogram to the store and returns the new store."
+  [store namespace metric help label-names buckets]
+  (assoc-in store
+            [:metrics namespace metric]
+            (make-histogram (:registry store) namespace metric help label-names buckets)))
+
+(defn track-observation
+  "Track the value of an observation for a registered histogram."
+  [store namespace metric value & {:keys [labels] :or {labels []}}]
+  (-> (histogram-with-labels (get-in store [:metrics namespace metric]) labels)
+      (.observe value)))
 
 (defn with-path
   "Adds the matched compojure route as the :path response metadata attribute"
